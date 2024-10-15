097f750b632f7790167177d2add451a8864b26a7
Martin Salted <martinsalted@gmail.com>
2024-10-15 23:38:31 +0200

    double click modify finished

--- src/app/components/common/double-click-modify/double-click-modify.component.ts
+++ src/app/components/common/double-click-modify/double-click-modify.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
   ListSelect,
@@ -15,18 +23,57 @@ import {
 export class DoubleClickModifyComponent {
   /** INPUTS */
   @Input() value: any; // la valeur à afficher ou modifier
-  @Input() optionList?: ListSelect[]; // les options à afficher
+  @Input() optionList?: ListSelect[]; // les options à affichersi présent on a une liste déroulante
+  @Input() toolTipText?: string; // texte à afficher dans le tooltip
+  @Input() showButtons?: boolean = false; // si true affiche les boutons
+
+  /** VIEWCHILDS */
+  @ViewChild('input') input!: ElementRef;
+  @ViewChild('listSelect') listSelect!: ElementRef;
 
   /** OUTPUTS */
   @Output() valueEmit = new EventEmitter<any>(); // renvoie la valeur du parent
 
-  valueModifiy: boolean = false;
-  toolTipText?: string;
+  /** variables de gestion */
+  valueModifiy: boolean = false; // true si la valeur est en cour de modification
 
+  /** émission de la valeur vers le parent */
   change(value: any) {
     if (this.valueModifiy) {
-      this.valueEmit.emit(this.value);
+      this.valueEmit.emit(value);
       this.valueModifiy = false;
     }
+  }
+
+  /** annulation via touche escape */
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscapeKeydown(event: KeyboardEvent) {
+    this.valueModifiy = false;
+  }
+
+  /** soumission avec la touche enter */
+  @HostListener('document:keydown.enter', ['$event'])
+  onEnterKeydown(event: KeyboardEvent) {
+    this.change(this.value);
+  }
+
+  /** gère le click en dehors du champs */
+  @HostListener('document:mousedown', ['$event'])
+  onGlobalClick(event: Event): void {
+    if (this.input) {
+      if (!this.input.nativeElement.contains(event.target)) {
+        this.valueModifiy = false;
+      }
+    }
+    if (this.listSelect) {
+      if (!this.listSelect.nativeElement.contains(event.target)) {
+        this.valueModifiy = false;
+      }
+    }
+  }
+
+  @HostListener('dblclick', ['$event'])
+  onDoubleClick(event: MouseEvent) {
+    this.valueModifiy = true;
   }
 }
